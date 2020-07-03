cbbeb1f1c41e6a2d49e2e49932874bb517589736
Samet Aslan <me@samet-aslan.de>
2020-07-03 13:50:44 +0200

    Update scrawler.py
    Minor changes

--- scrawler.py
+++ scrawler.py
@@ -2,18 +2,22 @@
 # Samet Aslan 2020
 from selenium import webdriver
 
+#Variables
 PATH = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
+mainurl = "https://www.shpock.com/"
+language = "de-de" #available de-at, en-gb
 keyword = "nintendo"
-url = "https://www.shpock.com/de-de/results?q=" + keyword + "&location.distance=1000&location.isDomestic=true&sort=d%3Adate_start"
+distance = "1000"
+url = mainurl + language + "/results?q=" + keyword + "&location.distance=1000&location.isDomestic=true&sort=d%3Adate_start"
+
+#
+driver = webdriver.Chrome(PATH)
 driver.get(url)
  
 #Test: get the first img of results
-#imagewrapper = driver.find_element_by_xpath("//*[@id='__next']/div[2]/div[3]/div[2]/div[1]/div/div/div[3]/div[1]/a/div/div/div[1]")
-#image = imagewrapper.find_element_by_tag_name("img").get_attribute("src")
-#print(image)
-
-#class="CardGrid-sc-1l8yw3u-0 fuVpoX"
+imagewrapper = driver.find_element_by_xpath("//*[@id='__next']/div[2]/div[3]/div[2]/div[1]/div/div/div[3]/div[1]/a/div/div/div[1]")
+image = imagewrapper.find_element_by_tag_name("img").get_attribute("src")
+print(image)
 
 #get the results wrapper
 xresult = "//*[@id='__next']/div[2]/div[3]/div[2]/div[1]/div/div/div[3]"
